7ac7ef3e1ee613db3ee6f79a18df7eb2c63dbcf7
Mufaddal Shakir <muffi606@gmail.com>
2020-11-06 06:36:57 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -4,17 +4,10 @@ import './index.css';
 
 class Square extends React.Component {
 
-  constructor(props){
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
   render() {
     return (
       <button className="square" 
-           onClick={() => this.setState({value: 'X'})}>
+           onClick={() => this.props.onClick()}>
         {this.props.value}
       </button>
     );
@@ -22,8 +15,26 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
+  constructor(props){
+      super(props);
+      this.state = {
+          squares: Array(9).fill(null),
+      };
+  }
+
+  handleClick(i){
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+  }
+  
+    renderSquare(i) {
+    return (
+            <Square
+              value={this.state.squares[i]}
+              onClick = {() => this.handleClick(i)} 
+            />
+            )
   }
 
 
